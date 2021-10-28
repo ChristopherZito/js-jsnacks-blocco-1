78c66a29a0039c52678f9935443c6509af57780a
ChristopherZito <87862547+ChristopherZito@users.noreply.github.com>
2021-10-28 18:09:56 +0200

    jsnacks6 sbatto

--- jsnack 6/js/script.js
+++ jsnack 6/js/script.js
@@ -1,1 +1,29 @@
-// Genera un array di 50 numeri random. Nell’array non devono esserci doppioni.
+// Genera un array di 50 numeri random.
+// Nell’array non devono esserci doppioni.
+
+// creazione array
+const conta = [];
+
+
+let num = Math.floor(Math.random() * 10) + 1;
+conta.push(num)
+console.log("numero uscito = " + num); 
+let found = false;
+for(let i = 0; i < 10 && found == false; i++){
+    if(num == conta[i]){
+        found == true;
+        conta.push(num)
+        console.log("numero caricato = " + num);
+        num = Math.floor(Math.random() * 10) + 1;
+        console.log("numero ricreato = " + num);
+    }/* else {
+        num = Math.floor(Math.random() * 10) + 1;
+        console.log("numero else = " + num);
+    } */
+}
+
+
+// inserimento in-page
+const text = document.getElementById("list");
+text.innerHTML +="numeri nell'array: " + conta;
+console.log("numeri nell'array: " + conta);
