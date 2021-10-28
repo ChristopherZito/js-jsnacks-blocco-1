43014dca7664cffd9ba6c09ddd1ad39bb124b4ed
ChristopherZito <87862547+ChristopherZito@users.noreply.github.com>
2021-10-28 21:19:12 +0200

    last hope

--- jsnack 6/js/script.js
+++ jsnack 6/js/script.js
@@ -4,26 +4,30 @@
 // creazione array
 const conta = [];
 
-
-let num = Math.floor(Math.random() * 10) + 1;
-conta.push(num)
-console.log("numero uscito = " + num); 
-let found = false;
-for(let i = 0; i < 10 && found == false; i++){
-    if(num == conta[i]){
-        found == true;
-        conta.push(num)
-        console.log("numero caricato = " + num);
-        num = Math.floor(Math.random() * 10) + 1;
-        console.log("numero ricreato = " + num);
-    }/* else {
-        num = Math.floor(Math.random() * 10) + 1;
-        console.log("numero else = " + num);
-    } */
+//creo 50 valori e li inserisco nell'array
+for(let i = 0; i <  50 ; i++){
+    let num = Math.floor(Math.random() * 100) + 1;
+    console.log("numero generato = " + num);
+    conta.push(num)
 }
+console.log("numeri attuali nell'array: " + conta);
 
+//provo a confrontare i numeri dell'array con i numeri nell'array stesso
+let verifica = false;
+for(let i = 0; i < conta.length  && verifica == false; i++){
+    let num = [...conta];
+    console.log("numero creato nel secondo for = " + num);
+    if(num == conta[i]){
+        conta.slice(1),conta[i];
+        let numNew = Math.floor(Math.random() * 100) + 1;
+        console.log("numero generato = " + num);
+        conta.push(numNew)
+    }else {
+        verifica = true;
+    }
+}
 
 // inserimento in-page
 const text = document.getElementById("list");
-text.innerHTML +="numeri nell'array: " + conta;
-console.log("numeri nell'array: " + conta);
+text.innerHTML +=" numeri nell'array: " + conta;
+console.log("numeri dell'array da mostrare: " + conta);
