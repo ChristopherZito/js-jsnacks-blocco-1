ac028303dc68f6b31a1ee973494e1d6f3f01d363
ChristopherZito <87862547+ChristopherZito@users.noreply.github.com>
2021-10-28 15:15:37 +0200

    jsnacks5 consegna

--- jsnack 5/js/script.js
+++ jsnack 5/js/script.js
@@ -1,3 +1,15 @@
 // Crea un array vuoto.
 // Chiedi per 6 volte all’utente di inserire un numero,
 // se è dispari inseriscilo nell’array.
+
+const num = [];
+
+for(let i = 0; i < 6 ; i++){
+    let miNum = parseInt(prompt("inserire un numero:"));
+    console.log("numero da me messo= " + miNum);
+    if (miNum % 2 !== 0){
+        num.push(miNum);
+    }
+}
+document.getElementById("bo").innerHTML = "i numeri inseriri sono: " + num;
+console.log("i numeri inseriti sono: " + num);
