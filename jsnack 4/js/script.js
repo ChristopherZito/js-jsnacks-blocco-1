eb7c22c2130cf3b806aa917dded6745c88bedae2
ChristopherZito <87862547+ChristopherZito@users.noreply.github.com>
2021-10-28 15:08:39 +0200

    jsnacks4 consegna

--- jsnack 4/js/script.js
+++ jsnack 4/js/script.js
@@ -1,1 +1,22 @@
-// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+// In un array sono contenuti i nomi degli invitati
+// alla festa del grande Gatsby,
+// chiedi all’utente il suo nome e
+// comunicagli se può partecipare o no alla festa.
+
+
+// lista invitati
+const nomi = ["luca", "marco", "filippo","Luca", "Marco", "Filippo"]
+// richiesta del nome
+const mioNome = prompt("inserisci il tuo nome")
+console.log("il nome è: " + mioNome);
+// conferma invito
+const text = document.getElementById("invito");
+let verifica = false;
+for(let i = 0; i < nomi.length && verifica == false; i++){
+    if(mioNome == nomi[i]){
+        verifica = true
+        text.innerHTML = "invito valido"
+    }else {
+        text.innerHTML = "invito non valido"
+    }
+}
